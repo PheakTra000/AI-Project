851e927b121b456402146a5f382fd115e4e9c6d7
tra <tralegendary99@gmail.com>
2025-08-18 15:23:16 +0700

    tired

--- chat/client.py
+++ chat/client.py
@@ -5,6 +5,39 @@ import threading
 HOST = '100.81.1.108'
 PORT = 1234
 
+
+def listen_for_messages_from_server(client):
+    while True:
+        message = client.recv(2048).decode('utf-8')
+        if message != '':
+            username = message.split(":")[0]
+            content = message.message.split(':')[1]
+
+            print(f"[{username}] {content}")
+        else:
+            print("Message received from client is empty")
+
+def send_message_to_server(client):
+    while True:
+        message = input("Message: ")
+        if message != '':
+            client.sendall(message.encode())
+        else:
+            print("empty message")
+            exit(0)
+
+def communicate_to_server(client):
+    username = input("Enter username: ")
+    if username != '':
+        client.sendall(username.encode())
+    else:
+        print("Username cannot be empty")
+        exit(0)
+
+    threading.Thread(target=listen_for_messages_from_server, args=(client, )).start()
+
+    send_message_to_server(client)
+
 def main():
     # creating a socket object
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -15,5 +48,7 @@ def main():
         print(f"Successfully connect to server on {HOST} {PORT}")
     except:
         print(f"Unable to connect to the server {HOST} {PORT}")
+
+    communicate_to_server(client)
 if __name__ == '__main__':
     main()
--- chat/server.py
+++ chat/server.py
@@ -4,6 +4,39 @@ import threading
 HOST = '100.81.1.108'
 PORT = 1234 # we can use any port from 0 to 65535
 LISTENER_LIMIT = 5
+active_clients = [] #list of all currently connected users
+
+#function to listen for upcoming messages from a client
+def listen_for_messages(client, username):
+    while True:
+        message = client.recv(2048).decode('utf-8')
+        if message != '':
+            final_msg = username + ': ' + message
+            send_messages_to_all(final_msg)
+        else:
+            print(f"The message send from client {username} is empty")
+
+#function to send the message to a single client
+def send_messages_to_client(client, message):
+    client.sendall(message.encode())
+
+# function to send message to all client that are currently connected to this server
+def send_messages_to_all(from_username, message):
+    for user in active_clients:
+        send_messages_to_client(user[1], message)
+
+
+#function to hand client
+def client_handler(client):
+
+    # server will listen for client message that will contain the username
+    while True:
+        username = client.recv(2048).decode('utf-8')
+        if username != '':
+            active_clients.append((username, client))
+        else:
+            print("Client name is empty")
+    threading.Thread(target=listen_for_messages, args=(client, username, )).start()
 
 def main():
     # creating the socket class object
@@ -27,6 +60,10 @@ def main():
 
         client, address = server.accept()
         print(f"Successfully connected to client{address[0]} {address[1]}")
+
+        threading.Thread(target=client_handler, args=(client, )).start()
+
+
 
 if __name__ == '__main__':
     main()
